87d8af445d5e8e5f98860f00c38ec02ad1b72134
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-11 16:58:59 +0200

    Added the registration API

--- Register.php
+++ Register.php
@@ -1,4 +1,28 @@
 <?php
+include("Connection.php");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
+$name = $_POST['name'];
+$dob = $_POST['dob'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$check_email = $mysqli -> prepare('select email from users where email=?');
+$check_email -> bind_param('s', $email);
+$check_email -> execute();
+$check_email -> store_result();
+$email_exists = $check_email->num_rows();
+
+$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+if($email_exists > 0){
+  $response['status'] = "Failed: Email Already Exists!";
+}else{
+  $query = $mysqli -> prepare('insert into users(name, email, password, dob, usertype_id) values(?,?,?,?,1)');
+  $query -> bind_param('ssss', $name, $email, $hashed_password, $dob);
+  $query -> execute();
+  $response['status'] = "User Added!";
+}
+
+echo json_encode($response);
