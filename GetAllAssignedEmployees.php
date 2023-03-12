fd477d8543e10cb81d995a7ed908812beaccace3
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-12 15:22:19 +0200

    Added the get all assigned employees API

---
+++ GetAllAssignedEmployees.php
@@ -0,0 +1,20 @@
+<?php
+include ('connection.php');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+$query = $mysqli->prepare("select hospital_users.id, users.id, users.name, hospitals.id, hospitals.name, hospital_users.date_joined, hospital_users.date_left
+from hospital_users 
+inner join hospitals on hospital_users.hospital_id = hospitals.id
+inner join users on hospital_users.user_id = users.id
+where users.usertype_id = 2
+");
+$query -> execute();
+$result = $query->get_result();
+
+$patients_in_hospitals = [];
+while ($row = $result->fetch_assoc()){
+  $patients_in_hospitals[] = $row;
+}
+
+echo json_encode($patients_in_hospitals);
