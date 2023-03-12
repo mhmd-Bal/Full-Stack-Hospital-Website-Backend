8a2c9ece142f0a9ce48b10132b59d45c38f40ee8
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-12 10:22:43 +0200

    Added the Login API with the jwt authentication

--- Login.php
+++ Login.php
@@ -9,21 +9,13 @@ use Firebase\JWT\Key;
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$key = 'example_key';
-$payload = [
-    'iss' => 'http://example.org',
-    'aud' => 'http://example.com',
-    'iat' => 1356999524,
-    'nbf' => 1357000000
-];
-
-$query = $mysqli->prepare('select id,name,email,password from users where email=?');
+$query = $mysqli->prepare('select id,name,email,usertype_id,password from users where email=?');
 $query->bind_param('s', $email);
 $query->execute();
 
 $query->store_result();
 $user_doesnt_exist = $query->num_rows();
-$query->bind_result($id, $name,$email,$hashed_password);
+$query->bind_result($id, $name,$email,$usertype_id,$hashed_password);
 $query->fetch();
 $response = [];
 
@@ -32,15 +24,20 @@ if ($user_doesnt_exist == 0) {
     
 } else {
     if (password_verify($password, $hashed_password)) {
-      $jwt = JWT::encode($payload, 'your_secret_key', 'HS256');
-      $response['token'] = $jwt;
-      // $response['response'] = "logged in";
-      // $response['email'] = $email;
-      // $response['id'] = $id;
-      // $response['name'] = $name;
-      // $response['email'] = $email;
+
+        $key = 'hospital_secret_key';
+        $payload = [];
+        $payload['sub'] = $email;
+        $payload['name'] = $name;
+        $payload['usertype'] = $usertype_id;
+
+        $jwt = JWT::encode($payload, $key, 'HS256');
+        $response['response'] = "logged in";
+        $response['token'] = $jwt;
+        $response['usertype'] = $usertype_id;
+
     } else {
-     $response["response"] = "Incorrect email or password";
+      $response["response"] = "Incorrect email or password";
     }
 }
  
