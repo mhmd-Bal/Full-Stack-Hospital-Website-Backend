9a323a3072e1abf96abeba62e93352d8796df62c
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-12 16:03:22 +0200

    Fixed the 2 getallassignedusers APIs

--- GetAllAssignedPatients.php
+++ GetAllAssignedPatients.php
@@ -3,7 +3,7 @@ include ('connection.php');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-$query = $mysqli->prepare("select hospital_users.id, users.id, users.name, hospitals.id, hospitals.name, hospital_users.date_joined, hospital_users.date_left
+$query = $mysqli->prepare("select hospital_users.id, hospital_users.user_id, users.user_name, hospital_users.hospital_id, hospitals.hospital_name, hospital_users.date_joined, hospital_users.date_left
 from hospital_users 
 inner join hospitals on hospital_users.hospital_id = hospitals.id
 inner join users on hospital_users.user_id = users.id
