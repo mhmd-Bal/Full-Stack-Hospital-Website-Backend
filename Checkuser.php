8883a6ef9714d8871ecad11f707db6f62fe8f315
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-12 11:03:25 +0200

    Added checking user using jwt

--- Checkuser.php
+++ Checkuser.php
@@ -1,19 +1,23 @@
 <?php
 include('connection.php');
 require_once('jwt/src/JWT.php');
+require_once('jwt/src/Key.php');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-$token = $_POST['token'];
-$mykey = "hospital_secret_key";
+$jwt = $_POST['token'];
+$key = "hospital_secret_key";
 $response = [];
 
-if(isset($token)){
-  $decoded = JWT::decode($token, new Key($mykey, 'HS256'));
+if(isset($jwt)){
+  $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
   $decoded_array = (array) $decoded;
   $response['Authentication'] = "Successful";
+  $response['name'] = $decoded_array['name'];
+  $response['email'] = $decoded_array['sub'];
+  $response['usertype'] = $decoded_array['usertype'];
 }else{
   $response['Authentication'] = "Failed";
 }
