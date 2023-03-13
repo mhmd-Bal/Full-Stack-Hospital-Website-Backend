5d1d93a4f0797be9c19f85e06ce300eeaf352519
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-13 02:55:38 +0200

    Added the DeclineRequest APi

--- DeclineRequest.php
+++ DeclineRequest.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
 $service_id = $_POST['service_id'];
-$status = "Approved";
+$status = "Declined";
 
 $check_service = $mysqli->prepare('select id from services where id=?');
 $check_service->bind_param('i', $service_id);
@@ -20,7 +20,7 @@ if ($service_doesnt_exist == 0) {
   $check_user = $mysqli->prepare('update services set status=? where id=?');
   $check_user->bind_param('si', $status, $service_id);
   $check_user->execute();
-  $response['response'] = "Service Approved!";
+  $response['response'] = "Service Declined!";
 }
  
 echo json_encode($response);
